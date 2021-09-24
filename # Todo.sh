56452984d43846ead562b83cb403700e118a5ab7
erstert3st <erstert3st@test.de>
2021-09-24 18:39:10 +0200

    addTodo

---
+++ # Todo.sh
@@ -0,0 +1,9 @@
+#   todo 
+#   auto update
+#   https:
+#          Nexus, Jenkins, Kubernetes, Docker ?  
+#   Kubernetes normal login after https 
+#
+#   testproject Ideas:
+#                      php: webserver, myphp, tester
+#                      nodejs: Backend, myphp, webserver, tester   
