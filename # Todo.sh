9d3d5a27d5c8abe06609bb09d1b2df68db744075
erstert3st <erstert3st@test.de>
2021-09-28 11:56:14 +0200

    lol

--- # Todo.sh
+++ # Todo.sh
@@ -7,3 +7,6 @@
 #   testproject Ideas:
 #                      php: webserver, myphp, tester
 #                      nodejs: Backend, myphp, webserver, tester   
+
+
+#! tipps https://github.com/zmarffy/jenkins-inbound-agent-with-docker
